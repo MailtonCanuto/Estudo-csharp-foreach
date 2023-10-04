218a5668e1e6227fc00f6cb13c854d45e9023b5e
Mailton Garrido Canuto <mgcfisio@hotmail.com>
2023-10-04 06:19:54 -0300

    Adicionar arquivos de projeto.

---
+++ Estudo csharp foreach/Program.cs
@@ -0,0 +1,18 @@
+﻿// comando basico foreach, percorre todos os elementos de um array
+
+string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+foreach (string car in cars)
+{
+    Console.WriteLine(car);
+}
+Console.WriteLine();
+
+int[] numbers = { 1, 2, 3, 4, 5 };
+int soma = 0;
+foreach (int number in numbers)
+{
+    
+    Console.Write($"Numero a ser somado = {soma } + {number} = ");
+    soma += number;
+    Console.WriteLine(soma);
+}
